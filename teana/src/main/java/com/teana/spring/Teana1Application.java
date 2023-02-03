ef5765bbcf8e041e90d483bb76425c406cf2283a
LeeEunJae <sbn04177@naver.com>
2023-02-03 21:00:25 +0900

    [update]

---
+++ teana/src/main/java/com/teana/spring/Teana1Application.java
@@ -0,0 +1,13 @@
+package com.teana.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teana1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teana1Application.class, args);
+	}
+
+}
---
+++ teana/src/test/java/com/teana/spring/Teana1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teana.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teana1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
